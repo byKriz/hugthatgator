7bbd43c4b53d4b9105afb9d8b27f91555c98a73e
byKriz <cristianesquivelh@hotmail.com>
2024-04-21 16:51:26 -0400

    agregando el adaptardor de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,6 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
