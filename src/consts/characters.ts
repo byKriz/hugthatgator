f76e0c1ed0013308b6ad34cbf72a3b61d5b845ae
byKriz <cristianesquivelh@hotmail.com>
2024-04-27 19:43:40 -0400

    creando los tipos y las imagenes

---
+++ src/consts/characters.ts
@@ -0,0 +1,45 @@
+import img from "../../public/characters/fb_carry.png";
+import type { CharacterType } from "../models/CharacterType";
+
+export const CHARACTERS: CharacterType[] = [
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+  {
+    id: "olivia",
+    name: "Olivia",
+    img: "",
+  },
+];
---
+++ src/models/CharacterType.ts
@@ -0,0 +1,5 @@
+export interface CharacterType {
+    id: string;
+    name: string;
+    img: string;
+}
